1c9cf1a1afc1975d3869e536d5be392f593a01fb
Witold Frącek <witek.fracek@gmail.com>
2022-01-17 14:11:27 +0100

    Started implementing user

--- Models/User.cs
+++ Models/User.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace PO_Projekt.Models
 {
     public class User
     {
+        [Required]
+        [Key]
+        [Range(0, int.MaxValue)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "")]
+        public string Email { get; set; }
     }
 }
