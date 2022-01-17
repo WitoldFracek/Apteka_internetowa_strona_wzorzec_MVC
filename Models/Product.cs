7e353a6a17688407172c81ba235445d02706284f
ksproska <kamila.sproska@gmail.com>
2022-01-17 14:10:29 +0100

    manifacturer, product, productname

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
 
 namespace PO_Projekt.Models
 {
     public class Product
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ProductName ProductName { get; set; }
+        [Required]
+        public int ProductNameId { get; set; }
     }
 }
--- Models/ProductName.cs
+++ Models/ProductName.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
 
 namespace PO_Projekt.Models
 {
     public class ProductName
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "To short name")]
+        [MaxLength(20, ErrorMessage = " To long name, do not exceed {1}")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0.0, double.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        [Required]
+        public string RequiresPrescription { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public Manufacturer Manufacturer { get; set; }
+        [Required]
+        public int ManufacturerId { get; set; }
     }
 }
