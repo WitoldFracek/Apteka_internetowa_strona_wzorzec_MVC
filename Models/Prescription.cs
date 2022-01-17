2c91e394bb8ae11471986311988dacc7951c6733
Michal Grzybowski <michalg.312@gmail.com>
2022-01-17 14:22:20 +0100

    addedPrescitpionsModels

--- Models/Prescription.cs
+++ Models/Prescription.cs
@@ -7,5 +7,20 @@ namespace PO_Projekt.Models
 {
     public class Prescription
     {
+        [Required]
+        [Key]
+        [Range(0, int.MaxValue)]
+        public int Id { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int PrescriptionCode { get; set; }
+        public DateTime StartDate { get; set }
+        public DateTime EndDate { get; set }
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+
     }
 }
--- Models/PrescriptionOrder.cs
+++ Models/PrescriptionOrder.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 namespace PO_Projekt.Models
 {
     public class PrescriptionOrder
-    {
+    
+        [Required]
+        [Key]
+        [Range(0, int.MaxValue)]
+        public int Id { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; 
+        [Required]
+        public int PrescriptionId { get; set; }
+        [ForeignKey("PrescriptionId")]
+        public Prescription Prescription { get; set; }
     }
 }
