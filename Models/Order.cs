237c0cace92f73a643b32b0ad91c22fbf4412f2b
Michal Grzybowski <michalg.312@gmail.com>
2022-01-17 14:12:14 +0100

    AddedOrderModel

--- Models/Order.cs
+++ Models/Order.cs
@@ -7,5 +7,21 @@ namespace PO_Projekt.Models
 {
     public class Order
     {
+        [Required]
+        [Key]
+        [Range(0, int.MaxValue)]
+        public int Id { get; set; }
+        [Required]
+        public int ShippingDataId { get; set; }
+        [ForeignKey("ShippingDataId")]
+        public ShippingData ShippingData { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public DateTime OrderDate { get; set }
+        public enum ShippingTypes { get; set; }
+
+
     }
 }
