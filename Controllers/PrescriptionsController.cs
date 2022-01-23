8fb30eb6d2bcc1d88e9818078e9c919d67994f5a
Witold Frącek <witek.fracek@gmail.com>
2022-01-23 18:26:44 +0100

    Dokumentacja dla klasy PrescriptionController

--- Controllers/PrescriptionsController.cs
+++ Controllers/PrescriptionsController.cs
@@ -11,14 +11,28 @@ namespace PO_Projekt.Controllers
 {
     public class PrescriptionsController : Controller
     {
+        /// <summary>
+        /// A database context that allows for the access to the local database.
+        /// </summary>
         private readonly ShopDbContext _context;
+        /// <summary>
+        /// Temporary field. In the future this parameter will be removed. It represents the logged in user.
+        /// </summary>
         private readonly int userId = 1;
 
+        /// <summary>
+        /// This is a constructor for PrescriptionController
+        /// </summary>
+        /// <param name="context">This parameter is a database context that allows the controller to access database objects</param>
         public PrescriptionsController(ShopDbContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// An async method that access data from database via context and redirects them into the Prescription Index View
+        /// </summary>
+        /// <returns>Prescription Index view of MVC structure</returns>
         [Route("Prescriptions")]
         public async Task<IActionResult> Index()
         {
@@ -34,11 +48,16 @@ namespace PO_Projekt.Controllers
             return View(firstPrescription);
         }
 
-        public IActionResult SearchPrescription()
-        {
-            return View();
-        }
+        //public IActionResult SearchPrescription()
+        //{
+        //    return View();
+        //}
 
+        /// <summary>
+        /// An async method that fetches prescription data specified by binded parameters of Prescription Class.
+        /// </summary>
+        /// <param name="pres">Prescription object with binded Code and Pesel parameters that is used to search the outsode prescription database for information about client prescriptions </param>
+        /// <returns>Redirects to Prescription Index View with received data from outsode database.</returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> SearchPrescription([Bind("Code,Pesel")] Prescription pres)
@@ -64,6 +83,13 @@ namespace PO_Projekt.Controllers
             return View("Index", generatedPrescription);
         }
 
+        /// <summary>
+        /// An async method that adds a new prescription to the database.
+        /// </summary>
+        /// <param name="code">Prescription code analogous to the real prescription code.</param>
+        /// <param name="startdate">Starting date from which the prescription is valid</param>
+        /// <param name="enddate">Prescripion expiary date</param>
+        /// <returns>Redirects to Prescription Index View</returns>
         public async Task<IActionResult> AddPrescription(int? code, string startdate, string enddate)
         {
             var newPrescription = new Prescription();
@@ -83,7 +109,12 @@ namespace PO_Projekt.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        
+
+        /// <summary>
+        /// An async method that removes the prescription specified by index from the local database. It does not affect prescriptions from outside source.
+        /// </summary>
+        /// <param name="id">Id of the prescription to be removed.</param>
+        /// <returns>Redirects to Prescription Index View</returns>
         public async Task<IActionResult> RemovePrescription(int? id)
         {
             Console.WriteLine(id);
@@ -104,6 +135,10 @@ namespace PO_Projekt.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        /// <summary>
+        /// A private async method that returns the list of prescriptions for a specified, logged in user by their id.
+        /// </summary>
+        /// <returns>Returns a list of not terminated prescriptions stored in the local database.</returns>
         private async Task<List<Prescription>> GetStoredPrescriptions()
         {
             var addedPrescriptions = await _context.Prescriptions
@@ -116,9 +151,17 @@ namespace PO_Projekt.Controllers
             return addedPrescriptions.Where(p => p.EndDate >= DateTime.Now).ToList();
         }
 
-        private void RemoveTerminated(List<Prescription> terminated)
+        /// <summary>
+        /// A private aync method that is used to remove expired prescriptions from the local database.
+        /// </summary>
+        /// <param name="terminated">The list of expired prescriptions to be removed.</param>
+        private async void RemoveTerminated(List<Prescription> terminated)
         {
-
+            foreach(var pres in terminated)
+            {
+                _context.Prescriptions.Remove(pres);
+            }
+            await _context.SaveChangesAsync();
         }
     }
 }
